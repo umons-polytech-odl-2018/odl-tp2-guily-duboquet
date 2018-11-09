ebe6c51e6ce0af8dac02bea8de62aeb7cc5beab5
Guily <thomas.guily1998@gmail.com>
2018-11-09 15:24:15 +0100

    First exercice almost finish

--- src/main/java/exercise1/Employee.java
+++ src/main/java/exercise1/Employee.java
@@ -1,15 +1,28 @@
 package exercise1;
 
-public class Employee {
-	public int computeSalary() {
-		return 0;
+public abstract class Employee {
+	protected String name;
+	protected int sales=0;
+	protected int workedHours=0;
+
+	Employee(String a)
+	{
+		name=a;
 	}
 
-	public void sell() { }
+	public abstract int computeSalary();
 
-	public void workOneHour() { }
+	public void sell() {
+		sales++;
+	}
+
+	public void workOneHour() {
+		workedHours++;
+	}
 
 	public String getName() {
-		return null;
+		return name;
 	}
 }
+
+
--- src/main/java/exercise1/Exercise1.java
+++ src/main/java/exercise1/Exercise1.java
@@ -2,14 +2,15 @@ package exercise1;
 
 public class Exercise1 {
 	public Employee buildFixedSalaryEmployee(String name, int fixedSalary) {
-		return null;
+		FixedSalaryEmployee a=new FixedSalaryEmployee(name, fixedSalary);
+		return a;
 	}
 
 	public Employee buildBaseSalaryPlusCommissionEmployee(String name, int baseSalary, int commission) {
-		return null;
+		return new BaseSalaryPlusCommissionEmployee(name, baseSalary, commission);
 	}
 
 	public Employee buildWorkHourSalaryEmployee(String name, int hourlyRoute) {
-		return null;
+		return new WorkHourSalaryEmployee(name, hourlyRoute);
 	}
 }
